0f9db0fed3650ea764f70c9ef4b20c1a7a3201c7
Isaac Moldofsky <isaac.moldofsky@gmail.com>
2014-08-16 23:16:23 -0500

    what is going on

--- run.py
+++ run.py
@@ -30,32 +30,6 @@ def ht:
 	else:
 		return 'Sorry, no subreddit found by that name. The sub could be inactive, maybe it migarted to a different name?'
 
-def pst:
-	return "post detected"
-	subreddit=sp[0]
-	post=sp[1]
-	num=sp[2]
-	try:
-		num=int(num)
-	except ValueError:
-		return 'Sorry, it seems like you didn\'t type the message right. Here\'s an example: LearnPython 2'
-	if post !='post':
-		return 'Sorry, it seems like you didn\'t type the message right. Here\'s an example: LearnPython 2'
-	if num>3:
-		return 'Uh oh. This could jam up the system; how about limiting it to the 3rd post?'
-	else if num<=0:
-		return 'Well, here are the 0 results you wanted!'
-	if subreddit=="random":
-		return get_post(r.get_subreddit(subreddit),num)
-	subs=r.search_reddit_names(subreddit)
-	if len(subs)>0:
-		for sub in subs:
-			if sub.display_name.lower()==subreddit:
-				return get_post(sub,num)
-		return 'Sorry, looks like I couldn\'t find that subreddit. Did you maybe mean reddit.com/r/%s? Or, the sub could be inactive. Maybe it moved to a different name?'%subs[0].display_name
-	else:
-		return 'Sorry, no subreddit found by that name. The sub could be inactive, maybe it migarted to a different name?'
-
 		
 def safe(s):
 	return str(s.encode("ascii", errors='ignore'))[2:-1]
@@ -66,7 +40,6 @@ def get_post(sub,num):
 	sumstr+="%s\n\n"%str(p.short_link.replace("http://",""))
 	return sumstr
 def get_posts(sub,num):
-	#actual subreddit object
 	sumstr="Hot posts on reddit.com/r/%s:\n\n"%sub.display_name
 	new_posts=sub.get_hot(limit=num)
 	for p in new_posts:
@@ -78,7 +51,7 @@ def get_posts(sub,num):
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-	msg=request.values.get('Body','- Wait a second... There\'s nothing in your message!?').lower()
+	msg=request.values.get('Body','Empty message?').lower()
 	resp = twilio.twiml.Response()
 	if msg=='wat' or msg=='what' or msg=='about' or msg=='help me' or msg=='hi' or msg=='hello':
 		resp.message("This is Isaac's (bit.ly/1pI0K9B) SMS Reddit parser application. \n Respond with the name of a subreddit and amount of results you want. Example: web_design 3 . put 'post' before the number to only get back the hot post with that ranking.")
